1a73d0ce8391ae0a1c45e430d3cc9a9904e4ca60
Jess Rose <jrosehow@gmail.com>
2023-04-14 22:54:41 -0400

    fix cache, generalize errors, cleanup code

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,70 @@
 import './App.css';
-// import Header from './Header';
-// import SearchBar from './SearchBar';
-// import Profile from './Profile';
 import { useEffect, useState } from 'react';
 
 const Profile = (props) => {
-  const user = props.user;
+  const { user, error } = props;
 
   return (
     <div className='profile'>
-      {!props.isUserFound && (
-        <div className='error'>
-          <h1>Error: User not Found</h1>
-        </div>
+      {error && (
+        <p className='error'>
+          <strong>Error:</strong> {error}
+        </p>
       )}
-      <ul>
-        <li>{user.name}</li>
-        <li>{user.login}</li>
-        <li>{user.created_at}</li>
-        <li>{user.bio}</li>
-      </ul>
+      {user && (
+        <ul>
+          <li>{user.name}</li>
+          <li>{user.login}</li>
+          <li>{user.created_at}</li>
+          <li>{user.bio}</li>
+          <li>
+            <em>Last updated: ${user.lastFetched}</em>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
 
 export default function App() {
   const [searchValue, setSearchValue] = useState('');
-  const [userData, setUserData] = useState('');
-  const [darkMode, setDarkMode] = useState(false);
+  const [userData, setUserData] = useState();
   const [userCache, setUserCache] = useState({});
-  const [isUserFound, setisUserFound] = useState(true);
+  const [fetchError, setFetchError] = useState();
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const searchUser = () => {
-    setisUserFound(true);
-    setUserData('');
+  const fetchUser = () => {
+    setUserData();
+    setFetchError();
 
     if (!searchValue) {
-      console.log('no value');
+      setFetchError('Empty username');
       return;
     }
 
-    if (!userCache[searchValue] == undefined) {
-      setUserData(searchValue);
+    const username = searchValue.toLowerCase();
+    const cachedUserData = userCache[username];
+
+    if (cachedUserData) {
+      console.log(`fetching ${username} from cache`);
+      setUserData(cachedUserData);
       return;
     }
+
     fetch(`https://api.github.com/users/${searchValue}`)
-      .then((response) => {
-        if (!response.ok) {
-          console.log('error loading');
-          return;
-        }
-        return response.json();
-      })
+      .then((response) =>
+        response.ok ? response.json() : setFetchError(`API ${response.status}`)
+      )
       .then((data) => {
-        if (!data) {
-          setisUserFound(false);
-          return;
+        if (data) {
+          setUserData(data);
+          setUserCache((prevUserCache) => ({
+            ...prevUserCache,
+            [username]: { ...data, lastFetched: Date.now() },
+          }));
+        } else {
+          setFetchError(`User '${searchValue}' not found`);
         }
-        setUserData(data);
-        setUserCache((prevUserCache) => ({
-          ...prevUserCache,
-          [data.login]: { ...data, lastFetched: Date.now() },
-        }));
       });
   };
 
@@ -69,8 +72,9 @@ export default function App() {
     console.log(userCache);
   }, [userCache]);
 
+  // TODO: implement dark mode
   const handleMode = () => {
-    setDarkMode((prevState) => !prevState);
+    setIsDarkMode((prevState) => !prevState);
   };
 
   return (
@@ -80,8 +84,8 @@ export default function App() {
         <div id='switch'>
           <input
             type='checkbox'
-            id='darkmode'
-            name='darkmode'
+            id='isDarkMode'
+            name='isDarkMode'
             onClick={handleMode}
           />
         </div>
@@ -91,82 +95,12 @@ export default function App() {
           type='text'
           name='search'
           onChange={(e) => setSearchValue(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              searchUser(searchValue);
-            }
-          }}
-          placeholder='Search GitHub username...'
+          onKeyDown={(e) => e.key === 'Enter' && fetchUser(searchValue)}
+          placeholder='Search GitHub username ...'
         />
-        <button onClick={searchUser}>Search</button>
+        <button onClick={fetchUser}>Search</button>
       </div>
-      <Profile user={userData} isUserFound={isUserFound} />
+      <Profile user={userData} error={fetchError} />
     </div>
   );
 }
-
-// useEffect(() => {
-//   console.log(`fetching ${selectedUser} ...`);
-//   fetch(`https://api.github.com/users/${selectedUser}`)
-//     .then((response) => response.json()
-//     .then((data) => setUserData(data));
-// }, [selectedUser]);
-
-// const searchUser = () => {
-//   if (!searchValue) return;
-
-//   console.log(`fetching ${searchValue} ...`);
-
-//   fetch(`https://api.github.com/users/${user}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setUserData((prevUserData) => ({
-//         ...prevUserData,
-//         [user]: { ...data, lastFetched: Date.now() },
-//       }));
-//       // setSelectedUser(searchValue);
-//     });
-// };
-
-// const selectUser = (user) => {
-
-//   if (user !== selectedUser) {
-//     setSelectedUser(user);
-//   }
-
-//   if (userData[user] && !isExpired(userData[user])) {
-//     console.log(`user ${user} already in state, skipping ...`);
-//     return;
-//   }
-
-//   doSelectUser(user);
-// };
-
-// const selectUserData = userData[selectedUser];
-
-// const SearchResult = (props) => {
-//   const data = props.data;
-//   if (props.data) {
-//     return (
-//       <ul>
-//         <li>Name: {data.name}</li>
-//         <li>Username: {data.login}</li>
-//         <li>Location: {data.location}</li>
-//       </ul>
-//     );
-//   } else {
-//     return <pre>No data.</pre>;
-//   }
-// };
-
-// const isExpired = (user) => {
-//   return false;
-
-//   // console.log(`isExpired`, user);
-//   // if (user.lastFetched <= 1681254591483) {
-//   //   console.log(`user ${user.login} is expired`);
-//   //   return true;
-//   // } else {
-//   //   return false;
-//   // }
-// };
